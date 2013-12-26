1d6b045fc43f5f9396faf98ed1a5d201c1414f30
Alvarex <deivid07.21@gmail.com>
2013-12-25 21:13:53 -0500

    App en Android

--- ColeApp/src/da/factory/LocalDaoFactory.java
+++ ColeApp/src/da/factory/LocalDaoFactory.java
@@ -22,7 +22,6 @@ public class LocalDaoFactory extends DAOFactory{
         InputStream iStream = null;
         try{
             HttpGet hGet = new HttpGet("http://tu_ip/Server/dao.interfaces/"+url);
-            //ServerLocal de Prueba 192.168.1.33
             HttpClient hClient = new DefaultHttpClient();
             HttpResponse response = (HttpResponse)hClient.execute(hGet);
             HttpEntity entity = response.getEntity();
